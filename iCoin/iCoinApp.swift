13fed33c25247fd7ffacc9dbc82c25947c156f75
Lin4 <lingesus@gmail.com>
2021-12-13 11:39:54 -0500

    iCoin project initial setup

---
+++ iCoin/iCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCoinApp.swift
+//  iCoin
+//
+//  Created by Lingeswaran Kandasamy on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct iCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
